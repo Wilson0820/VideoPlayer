75374ffb7c85981b5bc1721c8da086e368db1787
Joy0820 <309742645@qq.com>
2018-10-09 15:19:28 +0800

    add layout change on FolderList/MainActivity.java--1009

--- app/src/main/java/com/fxc/myvideoplayer/FolderList/MainActivity.java
+++ app/src/main/java/com/fxc/myvideoplayer/FolderList/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -20,6 +21,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.fxc.myvideoplayer.R;
+import com.fxc.myvideoplayer.VideoList.FileListActivity;
 import com.fxc.myvideoplayer.VideoList.VideoItems;
 
 import java.lang.reflect.Array;
@@ -30,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
     List<FolderItems> folderItems = new ArrayList<>();
     FolderAdapter adapter;
     String folderName,lastfolderName;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         adapter = new FolderAdapter(this,folderItems);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.addItemDecoration(new FolderItemDecoration(2));
@@ -54,13 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_menu);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"this is menu!",Toast.LENGTH_SHORT).show();
-            }
-        });
+
     }
 
     @Override
@@ -132,58 +131,31 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        getMenuInflater().inflate(R.menu.layout_change, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int id = item.getItemId();
+        if (id == R.id.action_layout_list) {
+            if (recyclerView.getLayoutManager() instanceof LinearLayoutManager){
+                item.setIcon(R.drawable.ic_list);
+                layoutManager = new StaggeredGridLayoutManager(
+                        2, StaggeredGridLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(layoutManager);
+                adapter.notifyDataSetChanged();
+                return true;
+            } else {
+                item.setIcon(R.drawable.ic_grid);
+                layoutManager = new LinearLayoutManager(this);
+                recyclerView.setLayoutManager(layoutManager);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+
+        }
         return true;
     }
 
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
-
-            //todo complete setting function in here
-
-            //Seamas-----------------------------------------------------------
-            Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-            String[] projections = {
-                    MediaStore.Video.Media.ALBUM
-            };
-            String order = MediaStore.Video.Media.DATE_MODIFIED + " DESC";
-            if (ActivityCompat.checkSelfPermission(
-                    MainActivity.this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-                ActivityCompat.requestPermissions(
-                        MainActivity.this,
-                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE,
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        5566);
-            try {
-                Cursor imageCursor = getContentResolver().query(
-                        uri,
-                        projections,
-                        null,
-                        null,
-                        order
-                );
-                if (imageCursor != null) {
-                    while (imageCursor.moveToNext()) {
-                        Log.d("Seamas", "album : " + imageCursor.getString(0));
-                    }
-                    imageCursor.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //-----------------------------------------------------------
-
-
-            return true;
-        } else if (id == R.id.action_search) {
-            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
 
 }
